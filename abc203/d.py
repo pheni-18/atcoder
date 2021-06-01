e954e5a7dba13b6ab2308d3cad1fcd2f44f6b0e2
pheni-18 <pheni.engineer@gmail.com>
2021-06-01 22:24:10 +0900

    abc203 d

--- abc203/d.py
+++ abc203/d.py
@@ -14,19 +14,41 @@ def inps(n, to_int=True):
 def main():
     n, k = inp()
     all = []
-
-    if n == 1:
-        a = inp()[0]
-        print(a)
-        return
-
-    for _ in [0] * n:
+    for _ in range(n):
         al = inp()
         all.append(al)
 
+    mi = ((k ** 2) // 2) + 1
+    wa = -1
+    ac = 1001001001
+    while wa + 1 < ac:
+        wj = (wa + ac) // 2
+        cll = [[0] * (n + 1) for _ in range(n + 1)]
+        for i in range(n):
+            for j in range(n):
+                cll[i + 1][j + 1] = 1 if all[i][j] > wj else 0
 
+        for i in range(n + 1):
+            for j in range(n):
+                cll[i][j + 1] += cll[i][j]
 
+        for i in range(n):
+            for j in range(n + 1):
+                cll[i + 1][j] += cll[i][j]
 
+        ok = False
+        for i in range(n - k + 1):
+            for j in range(n - k + 1):
+                cum = cll[i+k][j+k] - cll[i][j+k] - cll[i+k][j] + cll[i][j]
+                if cum < mi:
+                    ok = True
+
+        if ok:
+            ac = wj
+        else:
+            wa = wj
+
+    print(ac)
 
 
 if __name__ == '__main__':
