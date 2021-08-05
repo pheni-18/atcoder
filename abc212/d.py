b6c8e2db76063c25374caabbbf2f5983fc7ed904
pheni-18 <pheni.engineer@gmail.com>
2021-08-05 21:29:53 +0900

    abc212 d

--- abc212/d.py
+++ abc212/d.py
@@ -6,7 +6,26 @@ def inp(to_int=True):
 
 
 def main():
-    pass
+    import heapq
+
+    n = inp()[0]
+    ql = [inp() for _ in range(n)]
+
+    h = []
+    s = 0
+    for q in ql:
+        p = q[0]
+        x = None
+        if p == 1 or p == 2:
+            x = q[1]
+
+        if p == 1:
+            heapq.heappush(h, x - s)
+        elif p == 2:
+            s += x
+        else:
+            v = heapq.heappop(h)
+            print(v + s)
 
 
 if __name__ == '__main__':
