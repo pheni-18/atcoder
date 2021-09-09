f495978dce14a186eb33194694950bca1eb44c38
pheni-18 <pheni.engineer@gmail.com>
2021-09-09 22:54:19 +0900

    abc208 c

--- abc208/c.py
+++ abc208/c.py
@@ -6,7 +6,23 @@ def inp(to_int=True):
 
 
 def main():
-    pass
+    n, k = inp()
+    al = inp()
+    pl = []
+    for i in range(n):
+        a = al[i]
+        pl.append({'i': i, 'a': a, 'c': 0})
+
+    pl = sorted(pl, key=lambda x: x['a'])
+    r = k % n
+    for i in range(n):
+        pl[i]['c'] += k // n
+        if i < r:
+            pl[i]['c'] += 1
+
+    pl = sorted(pl, key=lambda x: x['i'])
+    for p in pl:
+        print(p['c'])
 
 
 if __name__ == '__main__':
