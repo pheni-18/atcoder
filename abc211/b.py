372ddf0f4706419c79cfce12df5b43075b8ad6be
pheni-18 <pheni.engineer@gmail.com>
2021-09-21 23:08:15 +0900

    abc211 b

--- abc211/b.py
+++ abc211/b.py
@@ -6,7 +6,12 @@ def inp(to_int=True):
 
 
 def main():
-    pass
+    s1, s2, s3, s4 = [inp(False)[0] for _ in range(4)]
+    s = {s1, s2, s3, s4}
+    if len(s) == 4:
+        print('Yes')
+    else:
+        print('No')
 
 
 if __name__ == '__main__':
