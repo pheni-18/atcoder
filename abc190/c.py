62def7a0a91de5f62b9e2b63156f792b8d9b4887
pheni-18 <pheni.engineer@gmail.com>
2021-10-01 20:57:17 +0900

    abc190 c

--- abc190/c.py
+++ abc190/c.py
@@ -6,7 +6,32 @@ def inp(to_int=True):
 
 
 def main():
-    pass
+    from itertools import product
+
+    n, m = inp()
+    abl = [inp() for _ in range(m)]
+    k = inp()[0]
+    cdl = [inp() for _ in range(k)]
+
+    ans = 0
+    for _bit in product((True, False), repeat=k):
+        pl = [False for _ in range(n + 1)]
+        for i in range(k):
+            c, d = cdl[i]
+            if _bit[i]:
+                pl[c] = True
+            else:
+                pl[d] = True
+
+        cnt = 0
+        for i in range(m):
+            a, b = abl[i]
+            if pl[a] and pl[b]:
+                cnt += 1
+
+        ans = max(ans, cnt)
+
+    print(ans)
 
 
 if __name__ == '__main__':
