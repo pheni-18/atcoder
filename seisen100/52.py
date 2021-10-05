58dc669532877a5b11018ef4bc15560e24f478b1
pheni-18 <pheni.engineer@gmail.com>
2021-10-05 21:48:32 +0900

    seisen100 52

---
+++ seisen100/52.py
@@ -0,0 +1,20 @@
+# https://atcoder.jp/contests/joi2017yo/tasks/joi2017yo_d
+
+# bitDP
+
+# TODO: solve
+
+
+def inp(to_int=True):
+    if not type(to_int) == bool:
+        raise Exception()
+    l = input().split()
+    return list(map(lambda x: int(x), l)) if to_int else l
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
