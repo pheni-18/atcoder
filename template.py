775193e48087566d3fb60e5ab64c035e8ef22106
pheni-18 <pheni.engineer@gmail.com>
2021-10-11 23:14:21 +0900

    Update template

--- template.py
+++ template.py
@@ -5,6 +5,13 @@ def inp(to_int=True):
     return list(map(lambda x: int(x), l)) if to_int else l
 
 
+def inp_one(to_int=True):
+    if not type(to_int) == bool:
+        raise Exception()
+    s = input()
+    return int(s) if to_int else s
+
+
 def main():
     pass
 
