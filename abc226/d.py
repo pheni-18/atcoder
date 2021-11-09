b69133e7c3d7cdcdeeb393f30186fd4398eaf671
pheni-18 <pheni.engineer@gmail.com>
2021-11-09 23:39:16 +0900

    abc226 d

--- abc226/d.py
+++ abc226/d.py
@@ -13,36 +13,25 @@ def inp_one(to_int=True):
 
 
 def main():
+    import math
+
     N = inp_one()
     xyl = [inp() for _ in range(N)]
 
     se = set()
-
-    zumi = set()
     for i in range(N):
+        x1, y1 = xyl[i]
         for j in range(N):
             if i == j:
                 continue
-            si = str(i).zfill(3)
-            sj = str(j).zfill(3)
-            lll = [si, sj]
-            lll = sorted(lll)
-            aaa = ''.join(lll)
-            if aaa in zumi:
-                continue
-            zumi.add(aaa)
-            x1, y1 = xyl[i]
-            x2, y2 = xyl[j]
-            xd = x1 - x2
-            yd = y1 - y2
-            xd += 1000000000
-            yd += 1000000000
-            xs = str(xd)
-            ys = str(yd)
-            s = xs.zfill(10) + ys.zfill(10)
-            se.add(s)
 
-    ans = len(se) * 2
+            x2, y2 = xyl[j]
+            dx, dy = x1 - x2, y1 - y2
+            g = math.gcd(dx, dy)
+            t = (dx // g, dy // g)
+            se.add(t)
+
+    ans = len(se)
     print(ans)
 
 
