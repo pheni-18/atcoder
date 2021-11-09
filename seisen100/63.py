a6b0bf32f306abbb6458a6a4308f93461cb704f9
pheni-18 <pheni.engineer@gmail.com>
2021-11-09 22:35:02 +0900

    seisen100 63

---
+++ seisen100/63.py
@@ -0,0 +1,27 @@
+# https://atcoder.jp/contests/abc074/tasks/arc083_b
+
+# ワーシャルフロイド法
+
+# TODO: Challenge
+
+
+def inp(to_int=True):
+    if not type(to_int) == bool:
+        raise Exception()
+    l = input().split()
+    return list(map(lambda x: int(x), l)) if to_int else l
+
+
+def inp_one(to_int=True):
+    if not type(to_int) == bool:
+        raise Exception()
+    s = input()
+    return int(s) if to_int else s
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
