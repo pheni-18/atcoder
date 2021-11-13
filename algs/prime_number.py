064bde1544dc045df2630165fe64241ba28f8d52
pheni-18 <pheni.engineer@gmail.com>
2021-11-13 19:16:48 +0900

    Add count_divisors

--- algs/prime_number.py
+++ algs/prime_number.py
@@ -12,3 +12,20 @@ def is_prime(n: int) -> bool:
             return False
 
     return True
+
+
+# 約数列挙
+
+def count_divisors(n: int) -> int:
+    l = []
+    for i in range(1, n):
+        if i * i > n:
+            break
+
+        if n % i == 0:
+            l.append(i)
+            if n // i != i:
+                l.append(n // i)
+
+    l = sorted(l)
+    return l
