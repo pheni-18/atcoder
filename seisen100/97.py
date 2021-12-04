72b851ff50d32de92a780e96435fc89016b1fa4c
pheni-18 <pheni.engineer@gmail.com>
2021-12-04 15:31:15 +0900

    seisen100 97

---
+++ seisen100/97.py
@@ -0,0 +1,30 @@
+# https://atcoder.jp/contests/abc150/tasks/abc150_d
+
+# 数学的な問題
+
+# TODO: Challenge
+
+
+def inp(to_int=True):
+    if not type(to_int) == bool:
+        raise Exception()
+    l = input().split()
+    return list(map(lambda x: int(x), l)) if to_int else l
+
+
+def inp_one(to_int=True):
+    if not type(to_int) == bool:
+        raise Exception()
+    s = input()
+    return int(s) if to_int else s
+
+
+def main():
+    N, M = inp()
+    al = inp()
+
+
+
+
+if __name__ == '__main__':
+    main()
