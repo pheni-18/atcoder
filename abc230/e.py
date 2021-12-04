07de46b089b86e71dd86b0db0fbb3cab087ac5df
pheni-18 <pheni.engineer@gmail.com>
2021-12-04 11:27:45 +0900

    abc230 e

--- abc230/e.py
+++ abc230/e.py
@@ -13,7 +13,17 @@ def inp_one(to_int=True):
 
 
 def main():
-    pass
+    N = inp_one()
+    
+    ans = 0
+    i = 1
+    while i <= N:
+        x = N // i
+        ni = N // x + 1
+        ans += x * (ni - i)
+        i = ni
+
+    print(ans)
 
 
 if __name__ == '__main__':
